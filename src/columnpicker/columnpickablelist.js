2e444505cf90cf1228d31f54f87b0d88b1601ea6
Jan Martin Langeland <lodott@gmail.com>
2017-10-24 20:54:49 +0200

    Revert "Prettified the ui slightly."
    This reverts commit 3b356dbbe54fd5c28a62a545d068a9c5061f43df.

--- src/columnpicker/columnpickablelist.js
+++ src/columnpicker/columnpickablelist.js
@@ -1,21 +1,11 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { List, Datagrid } from 'admin-on-rest';
-import FlatButton from 'material-ui/FlatButton';
-import Popover from 'material-ui/Popover';
 import ColumnPicker from './columnpicker';
 
 import { toggleColumnPickerStatusAction, initializeColumnPickerAction } from './actions';
 
 export class ColumnPickableList extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state  = {
-            open: false,
-        };
-    }
 
     componentWillMount() {
         let columnSourceNames = [];
@@ -40,22 +30,6 @@ export class ColumnPickableList extends React.Component {
     }
     */
 
-    handleTouchTap = (event) => {
-        // This prevents ghost click.
-        event.preventDefault();
-
-        this.setState({
-            open: true,
-            anchorEl: event.currentTarget,
-        });
-    };
-
-    handleRequestClose = () => {
-        this.setState({
-            open: false,
-        });
-    };
-
     removeHiddenColumns(children) {
         return React.Children.map(children, (child) => {
             if (!child.props.source) {
@@ -78,17 +52,7 @@ export class ColumnPickableList extends React.Component {
 
         return (
             <div className="columnpickable-list">
-                <FlatButton label="Hide columns" secondary onClick={this.handleTouchTap}/>
-                <Popover
-                    open={this.state.open}
-                    anchorEl={this.state.anchorEl}
-                    anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-                    targetOrigin={{horizontal: 'left', vertical: 'top'}}
-                    onRequestClose={this.handleRequestClose}
-                    style={{ padding: '1rem' }}
-                >
-                    <ColumnPicker columns={this.props.columnsDisplayed} onCheckboxChanged={this.props.handleCheckboxChanged} />
-                </Popover>
+                <ColumnPicker columns={this.props.columnsDisplayed} onCheckboxChanged={this.props.handleCheckboxChanged} />
                 <Datagrid {...rest}>
                     {childrenToRender}
                 </Datagrid>
